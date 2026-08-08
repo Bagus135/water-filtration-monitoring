571181251095e8eb32895028bf30cf62bb8ece7b
Fbeye04 <visualjutsu610@gmail.com>
2026-08-08 22:07:50 +0700

    perubahan navbar sama status

--- frontend/src/app/component.tsx
+++ frontend/src/app/component.tsx
@@ -1,13 +1,31 @@
-"use client"
+"use client";
 
-export function MetricRow({name, value, unit} : {name : string, value: string, unit? : string}){
-    return(
-        <div className="flex items-baseline justify-between border-b border pb-[0.6rem] last:border-b-0 last:pb-0">
-            <span className="text-[0.85rem]">{name}</span>
-            <span className="font-mono text-[1.4rem] font-medium">
-                {value}
-                {unit && <span className="text-[0.75rem] ml-1"> {unit}</span>}
-            </span>
+export function MetricRow({
+  name,
+  value,
+  unit,
+  icon,
+  variant,
+}: {
+  name: string;
+  value: string;
+  unit?: string;
+  icon: React.ReactNode;
+  variant: "blue" | "green";
+}) {
+  return (
+    <div className='flex items-baseline justify-between border-b border pb-[0.6rem] last:border-b-0 last:pb-0'>
+      <span className='text-[0.85rem]'>
+        <div
+          className={`p-2 rounded-full bg-opacity-20 ${variant === "blue" ? "bg-blue-500" : "bg-green-500"}`}>
+          {icon}
         </div>
-    )
+        {name}
+      </span>
+      <span className='font-mono text-[1.4rem] font-medium'>
+        {value}
+        {unit && <span className='text-[0.75rem] ml-1'> {unit}</span>}
+      </span>
+    </div>
+  );
 }
--- frontend/src/hooks/useReadSocket.ts
+++ frontend/src/hooks/useReadSocket.ts
@@ -1,45 +1,51 @@
 "use client";
 
-import {useEffect , useRef, useState} from "react"; 
-import { UseWaterQualitySocketResult, WaterQualityClientsReading } from "../types/types";
+import { useEffect, useRef, useState } from "react";
+import {
+  UseWaterQualitySocketResult,
+  WaterQualityClientsReading,
+} from "../types/types";
 
+export function useWaterQualitySocket(): UseWaterQualitySocketResult {
+  const [reading, setReading] = useState<WaterQualityClientsReading | null>(
+    null,
+  );
+  const [isESP32Connected, setESP32IsConnected] = useState<boolean>(false);
+  const wsRef = useRef<WebSocket | null>(null);
 
-export function useWaterQualitySocket() : UseWaterQualitySocketResult{
-    const [reading, setReading] = useState<WaterQualityClientsReading | null> (null); 
-    const [isESP32Connected, setESP32IsConnected] = useState<boolean>(false); 
-    const wsRef = useRef<WebSocket |null>(null);
-    
-    useEffect(()=>{
-        const protocol = window.location.protocol === "https:" ? "wss:" : "ws:"; 
-        const wsUrl = `${protocol}//${window.location.host}/ws`;
+  useEffect(() => {
+    const protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
+    const wsUrl =
+      process.env.NEXT_PUBLIC_WS_URL ||
+      `${protocol}//${window.location.host}/ws`;
 
-        const ws = new WebSocket(wsUrl); 
-        wsRef.current = ws; 
+    const ws = new WebSocket(wsUrl);
+    wsRef.current = ws;
 
-        ws.onopen = () =>{
-            console.log("WebSocket terhubung");
-            if(wsRef.current !== ws) return; 
-        };
+    ws.onopen = () => {
+      console.log("WebSocket terhubung");
+      if (wsRef.current !== ws) return;
+    };
 
-        ws.onmessage = (e)=>{
-            if(wsRef.current !== ws ) return; 
-    
-            const msg = JSON.parse(e.data); 
+    ws.onmessage = (e) => {
+      if (wsRef.current !== ws) return;
 
-            if (msg.type == "reading"){
-                setReading(msg.data as WaterQualityClientsReading); 
-            } else if (msg.type === "status"){
-                setESP32IsConnected(Boolean(msg.espcount));
-            }
-        }
+      const msg = JSON.parse(e.data);
 
-        ws.onclose = () => {
-            console.log("WebSocket terputus"); 
-            setESP32IsConnected(false);
-        }; 
+      if (msg.type == "reading") {
+        setReading(msg.data as WaterQualityClientsReading);
+      } else if (msg.type === "status") {
+        setESP32IsConnected(Boolean(msg.espcount));
+      }
+    };
 
-        return () => ws.close();
-    }, []);
+    ws.onclose = () => {
+      console.log("WebSocket terputus");
+      setESP32IsConnected(false);
+    };
 
-    return {reading,  isESP32Connected}
+    return () => ws.close();
+  }, []);
+
+  return { reading, isESP32Connected };
 }
