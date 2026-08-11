1b6a8c339bb15304876eccb467a7572aae028d8b
Bagus M <bagusmustaqim35@gmail.com>
2026-08-11 22:28:36 +0700

    add env.h example

---
+++ esp32/src/env.h
@@ -0,0 +1,10 @@
+#pragma once 
+
+const char* WIFI_SSID = "myITS-WiFi";
+const char* WIFI_IDENTITY = "xxx@student.its.ac.id";
+const char* WIFI_USERNAME = "xxx@student.its.ac.id";
+const char* WIFI_PASSWORD = "xxx";
+
+const char* device_token = "fisikaits123";
+
+
